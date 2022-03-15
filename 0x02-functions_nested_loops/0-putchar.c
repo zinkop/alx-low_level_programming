e69bdd80aff25f868c07875a5a5adf98e0913a2f
Zinkop <agalabri.alex@gmail.com>
2022-03-15 14:22:20 -0700

    commited

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the string "Holberton" from a character array.
+ * Return: 0 on success.
+ */
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
